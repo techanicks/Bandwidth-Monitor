4a958cdb8136f8d38d123bd6416c516638796b36
techanicks <orgahemengeorge@gmail.com>
2024-10-25 14:51:15 +0100

    Fix Procfile and port binding

--- speed.py
+++ speed.py
@@ -68,5 +68,6 @@ def download_results():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
